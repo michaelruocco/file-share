839ae22826024e034376fb12445fdcd486c0a04c
Michael Ruocco <michael.ruocco@hotmail.com>
2026-05-21 08:04:30 +0100

    adds an initial hello world lambda

---
+++ infra/lambda.tf
@@ -0,0 +1,39 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambda/dist"
+  output_path = "${path.module}/.terraform/lambda.zip"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "file-share-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello_lambda" {
+  function_name = "file-share-hello"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs22.x"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  timeout = 10
+
+  depends_on = [aws_iam_role_policy_attachment.lambda_basic_logs]
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,7 @@
 output "bucket_name" {
   value = aws_s3_bucket.file_share.bucket
 }
+
+output "lambda_name" {
+  value = aws_lambda_function.hello_lambda.function_name
+}
