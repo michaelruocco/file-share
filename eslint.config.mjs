cbc0b4dae4379daaa2b286e9e8b40eda315d20b7
Michael Ruocco <michael.ruocco@hotmail.com>
2026-05-29 05:44:07 +0100

    ignore dist from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,14 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
+    {
+    ignores: [
+      '**/dist/**',
+      '**/node_modules/**',
+      '**/coverage/**',
+      '**/playwright-report/**'
+    ]
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
